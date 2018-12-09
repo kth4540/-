b1c8e27022b6af2ff41bd855f280cd9258cecd40
kth4540 <43128350+kth4540@users.noreply.github.com>
2018-12-09 20:56:32 +0900

    앵간치

--- 소스.cpp
+++ 소스.cpp
@@ -16,25 +16,33 @@ struct BALL
 	float force;
 };
 
+float width;
+float height;
+
 float yangle = 135;
 float camangle = 270;
 
 int choose = 0;
 
-BALL blue_ball[5] = { { -60,-60 },{ 0,-60 },{ 60,-60 },{ -30,-30 },{ 30,-30 } };
-BALL white_ball[5] = { { -60,60 },{ 0,60 },{ 60,60 },{ -30,30 },{ 30,30 } };
+float block_x[4];
+float block_y[4];
+
+
+BALL blue_ball[5] = { { -60,-60 },{ 0,-60 },{ 60,-60 },{ -30,-60 },{ 30,-60 } };
+BALL white_ball[5] = { { -60,60 },{ 0,60 },{ 60,60 },{ -30,60 },{ 30,60 } };
 
 bool white_shot_check[5];
 bool blue_shot_check[5];
 
 bool turn = false;
-
+bool mode = false;
 void Keyboard(unsigned char key, int x, int y);
 void Timer(int value);
 
 
 float GetAngle(int ball1, int ball2);
 float Same_GetAngle(int ball1, int ball2);
+float Block_GetAngle(int ball, int block);
 
 void main(int argc, char *argv[])
 {
@@ -55,99 +63,165 @@ GLvoid drawScene(GLvoid)
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 바탕색을 'blue' 로 지정
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 전체를 칠하기
 	glEnable(GL_DEPTH_TEST);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt((100 * sin(PI / 180 * camangle)), (100 * sin(PI / 180 * yangle)), (100 * cos(PI / 180 * camangle)), 0, 0, 0, 0, 1, 0);
 
-	glPushMatrix();
+	if (mode == false)
 	{
-		glTranslatef(0, -20, 0);
+		glViewport(0, 0, width, height);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-400, 400, -300, 300, -800, 800);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
 		glColor3f(0.74, 0.62, 0.35);
-		glScalef(1, 0.1, 1);
-		glutSolidCube(200);
+		glRectf(-100, -100, 100, 100);
+
 		glColor3f(0, 0, 0);
-		glutWireCube(201);
-	}
-	glPopMatrix();	// 바둑판
-
-	glColor3f(0, 0, 0);
-
-	glPushMatrix();
-	{
+		glLineWidth(1);
 		for (int i = 0; i < 19; ++i)
 		{
 			glBegin(GL_LINES);
 			{
-				glVertex3f(-90, -9, -90 + (10 * i));
-				glVertex3f(90, -9, -90 + (10 * i));
+				glVertex3f(-90, 90 - (10 * i), 10);
+				glVertex3f(90, 90 - (10 * i), 10);
 			}
 			glEnd();
 
 			glBegin(GL_LINES);
 			{
-				glVertex3f(-90 + (10 * i), -9, -90);
-				glVertex3f(-90 + (10 * i), -9, 90);
+				glVertex3f(-90 + (10 * i), 90, 10);
+				glVertex3f(-90 + (10 * i), -90, 10);
 			}
 			glEnd();
-		}
-	}
-	glPopMatrix();	// 바둑판 줄
 
-	for (int i = 0; i < 5; ++i)
-	{
-		glColor3f(1, 1, 1);
-		glPushMatrix();
+		}
+
+		glColor3f(1, 0, 0);
+		glLineWidth(10);
+		glBegin(GL_LINE_STRIP);
 		{
-			glTranslatef(white_ball[i].x, 0, white_ball[i].y);
-			glScalef(1, 0.5, 1);
-			glutSolidSphere(5, 20, 20);
+			glVertex3f(-40, 40, 20);
+			glVertex3f(40, 40, 20);
+			glVertex3f(40, -40, 20);
+			glVertex3f(-40, -40, 20);
+			glVertex3f(-40, 40, 20);
 		}
-		glPopMatrix();
-
-		glColor3f(0, 0, 1);
-		glPushMatrix();
-		{
-			glTranslatef(blue_ball[i].x, 0, blue_ball[i].y);
-			glScalef(1, 0.5, 1);
-			glutSolidSphere(5, 20, 20);
-		}
-		glPopMatrix();
-	}	// 바둑알
-
-	glPushMatrix();
-	{
+		glEnd();
 		glColor3f(0, 1, 0);
-		if (turn == false)
-			glTranslatef(white_ball[choose].x, 20, white_ball[choose].y);
-		else
-			glTranslatef(blue_ball[choose].x, 20, blue_ball[choose].y);
-		if (turn == false)
-			glRotatef(180, 1, 0, 0);
-
-		if (turn == false)
-			glRotatef(-1 * white_ball[choose].angle, 0, 1, 0);
-		else
-			glRotatef(blue_ball[choose].angle, 0, 1, 0);
-		if (turn == false)
-			glScalef(1 + white_ball[choose].force, 1 + white_ball[choose].force, 1 + white_ball[choose].force);
-		else
-			glScalef(1 + blue_ball[choose].force, 1 + blue_ball[choose].force, 1 + blue_ball[choose].force);
-		glutSolidCone(5, 10, 10, 10);
+		for (int i = 0; i < 4;++i)
+		{
+			glPushMatrix();
+			glTranslatef(block_x[i], block_y[i], 30);
+			glutSolidSphere(6,6,6);
+			glPopMatrix();
+		}
+		
 	}
-	glPopMatrix();
+	else if (mode == true)
+	{
+		glViewport(0, 0, width, height);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(60.0f, width / height, 1.0, 1000.0);
+		glTranslatef(0.0, 0.0, -300.0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt((100 * sin(PI / 180 * camangle)), (100 * sin(PI / 180 * yangle)), (100 * cos(PI / 180 * camangle)), 0, 0, 0, 0, 1, 0);
+
+		glPushMatrix();
+		{
+			glTranslatef(0, -20, 0);
+			glColor3f(0.74, 0.62, 0.35);
+			glScalef(1, 0.1, 1);
+			glutSolidCube(200);
+			glColor3f(0, 0, 0);
+			glutWireCube(201);
+		}
+		glPopMatrix();	// 바둑판
+
+		glColor3f(0, 0, 0);
+		glLineWidth(1);
+		glPushMatrix();
+		{
+			for (int i = 0; i < 19; ++i)
+			{
+				glBegin(GL_LINES);
+				{
+					glVertex3f(-90, -9, -90 + (10 * i));
+					glVertex3f(90, -9, -90 + (10 * i));
+				}
+				glEnd();
+
+				glBegin(GL_LINES);
+				{
+					glVertex3f(-90 + (10 * i), -9, -90);
+					glVertex3f(-90 + (10 * i), -9, 90);
+				}
+				glEnd();
+			}
+		}
+		glPopMatrix();	// 바둑판 줄
+
+		for (int i = 0; i < 5; ++i)
+		{
+			glColor3f(1, 1, 1);
+			glPushMatrix();
+			{
+				glTranslatef(white_ball[i].x, 0, white_ball[i].y);
+				glScalef(1, 0.5, 1);
+				glutSolidSphere(5, 20, 20);
+			}
+			glPopMatrix();
+
+			glColor3f(0, 0, 1);
+			glPushMatrix();
+			{
+				glTranslatef(blue_ball[i].x, 0, blue_ball[i].y);
+				glScalef(1, 0.5, 1);
+				glutSolidSphere(5, 20, 20);
+			}
+			glPopMatrix();
+		}	// 바둑알
+
+		glPushMatrix();
+		{
+			glColor3f(0, 1, 0);
+			if (turn == false)
+				glTranslatef(white_ball[choose].x, 20, white_ball[choose].y);
+			else
+				glTranslatef(blue_ball[choose].x, 20, blue_ball[choose].y);
+			if (turn == false)
+				glRotatef(180, 1, 0, 0);
+
+			if (turn == false)
+				glRotatef(-1 * white_ball[choose].angle, 0, 1, 0);
+			else
+				glRotatef(blue_ball[choose].angle, 0, 1, 0);
+			if (turn == false)
+				glScalef(1 + white_ball[choose].force, 1 + white_ball[choose].force, 1 + white_ball[choose].force);
+			else
+				glScalef(1 + blue_ball[choose].force, 1 + blue_ball[choose].force, 1 + blue_ball[choose].force);
+			glutSolidCone(5, 10, 10, 10);
+		}
+		glPopMatrix();
+
+		for (int i = 0; i < 4; ++i)
+		{
+			glPushMatrix();
+			glTranslatef(block_x[i], 20, block_y[i]);
+			glRotatef(90, 1, 0, 0);
+			glutSolidCylinder(5, 30, 15, 15);
+			glPopMatrix();
+		}
+	}
 
 
 	glutSwapBuffers(); // 화면에 출력하기
 }
 GLvoid Reshape(int w, int h)
 {
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60.0f, w / h, 1.0, 1000.0);
-	glTranslatef(0.0, 0.0, -300.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	width = w;
+	height = h;
 
 	for (int i = 0; i < 5; ++i)
 	{
@@ -163,6 +237,12 @@ GLvoid Reshape(int w, int h)
 		blue_ball[i].cos = 1;
 		blue_ball[i].sin = 0;
 		blue_ball[i].force = 0;
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		block_x[i] = rand() % 80 - 40;
+		block_y[i] = rand() % 80 - 40;
 	}
 }
 
@@ -240,9 +320,26 @@ void Keyboard(unsigned char key, int x, int y)
 		else
 			blue_shot_check[choose] = true;
 		break;
+
+	case '=':
+		if (mode == false)
+			mode = true;
+		else
+			mode = false;
+		break;
+	case 'r':
+		for (int i = 0; i < 4; ++i)
+		{
+			block_x[i] = rand() % 80 - 40;
+			block_y[i] = rand() % 80 - 40;
+		}
 	}
+
 	glutPostRedisplay();
 }
+
+
+
 
 void Timer(int value)
 {
@@ -318,7 +415,6 @@ void Timer(int value)
 					blue_ball[j].cos = sin(GetAngle(i, j));
 					blue_ball[j].sin = cos(GetAngle(i, j));
 					blue_shot_check[j] = true;
-					printf("%f %f\n", white_ball[i].x, white_ball[i].y);
 				}
 
 				if (i != j && pow(white_ball[i].x - white_ball[j].x, 2) + pow(white_ball[i].y - white_ball[j].y, 2) < 100)
@@ -326,6 +422,15 @@ void Timer(int value)
 					white_ball[i].cos = sin(Same_GetAngle(i, j));
 					white_ball[i].sin = cos(Same_GetAngle(i, j));
 				}
+			}
+			for (int j = 0; j < 4; ++j)
+			{
+				if (pow(white_ball[i].x - block_x[j], 2) + pow(white_ball[i].y - block_y[j], 2) < 100)
+				{
+					white_ball[i].cos = -sin(Block_GetAngle(i, j));
+					white_ball[i].sin = -cos(Block_GetAngle(i, j));
+				}
+
 			}
 
 		}
@@ -346,10 +451,29 @@ void Timer(int value)
 					blue_ball[i].sin = cos(Same_GetAngle(j, i));
 				}
 			}
+			for (int j = 0; j < 4; ++j)
+			{
+				if (pow(blue_ball[i].x - block_x[j], 2) + pow(blue_ball[i].y - block_y[j], 2) < 100)
+				{
+					blue_ball[i].cos = sin(Block_GetAngle(i, j));
+					blue_ball[i].sin = cos(Block_GetAngle(i, j));
+				}
+
+			}
 
 		}
 	}
 
+
+	for (int i = 0; i < 5; ++i)
+	{
+		if (white_ball[i].x > 100 || white_ball[i].x < -100 || white_ball[i].y>100 || white_ball[i].y < -100)
+		{
+			white_ball[i].x = 0;
+			white_ball[i].y = 0;
+
+		}
+	}
 	glutPostRedisplay();
 	glutTimerFunc(10, Timer, 1);
 }
@@ -381,6 +505,29 @@ float Same_GetAngle(int ball1, int ball2)
 	{
 		deltaX = blue_ball[ball2].x - blue_ball[ball1].x;
 		deltaY = blue_ball[ball2].y - blue_ball[ball1].y;
+	}
+	angle = acosf(deltaX / 10);
+
+	if (deltaY < 0)
+		angle = 3.14 + (3.14 - angle);
+
+	return angle;
+}
+
+float Block_GetAngle(int ball, int block)
+{
+	float angle;
+	float deltaX;
+	float deltaY;
+	if (turn == false)
+	{
+		deltaX = white_ball[ball].x - block_x[block];
+		deltaY = white_ball[ball].y - block_y[block];
+	}
+	else if (turn == true)
+	{
+		deltaX = blue_ball[ball].x - block_x[block];
+		deltaY = blue_ball[ball].y - block_y[block];
 	}
 	angle = acosf(deltaX / 10);
 
